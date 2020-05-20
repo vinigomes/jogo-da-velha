496f67f10498565321778741ea7f320a3b85e43f
Vinícius Roggia Gomes <vinicius@intellead.com>
2020-05-19 22:10:07 -0300

    added build

---
+++ build/precache-manifest.29ffbae156cf9346596349db724afa31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4518bbbc1d353c4f7de2f78dc1418a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b9b0c150a79aa211a58",
+    "url": "/static/css/main.278145d6.chunk.css"
+  },
+  {
+    "revision": "3479e412842622cab3b8",
+    "url": "/static/js/2.63de4c66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63de4c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9b0c150a79aa211a58",
+    "url": "/static/js/main.ff43cbc3.chunk.js"
+  },
+  {
+    "revision": "3ad2ba30aa2ae95a5fc4",
+    "url": "/static/js/runtime-main.5cbdbddc.js"
+  }
+]);
